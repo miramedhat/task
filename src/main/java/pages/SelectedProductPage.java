2b819e5dbac41b755c772257c4cfc70e9848e675
Mira.Medhat Hanna <mira.medhat@swvl.com>
2021-08-26 13:25:03 +0200

    adding scroll to view method in Select Product Page

--- src/main/java/pages/SelectedProductPage.java
+++ src/main/java/pages/SelectedProductPage.java
@@ -2,21 +2,27 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 public class SelectedProductPage {
     private WebDriver driver;
     public By addToCart = By.cssSelector("[id=add_to_cart] button[type=submit]");
-    public By proceedToCheckOt = By.cssSelector("[class=\"button-container\"] a[class=\"btn btn-default button button-medium\"]");
+    public By proceedToCheckOut = By.cssSelector("[class=\"button-container\"] a[class=\"btn btn-default button button-medium\"]");
 
     public SelectedProductPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public ShoppingCartPage addToCart() throws InterruptedException {
+    public ShoppingCartPage addToCart()  {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", driver.findElement(addToCart));
+       // new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOf(driver.findElement(addToCart)));
         driver.findElement(addToCart).click();
         String parentWindowHandler = driver.getWindowHandle();
         String subWindowHandler = null;
@@ -26,7 +32,7 @@ public class SelectedProductPage {
             subWindowHandler = iterator.next();
         }
         driver.switchTo().window(subWindowHandler);
-        driver.findElement(proceedToCheckOt).click();
+        driver.findElement(proceedToCheckOut).click();
         return new ShoppingCartPage(driver);
     }
 }
